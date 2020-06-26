381b5051aa6fb17a77322af58d67a796377aabe0
enyaw15 <enyaw0215@gmail.com>
2020-06-25 20:18:24 -0700

    cleaned up some names

---
+++ Assets/Trains/Scripts/TrackData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class TrackData
+{
+    [SerializeField]
+    public Vector3[] points;
+
+    public TrackData(Vector3 start, Vector3 bPoint, Vector3 end)
+    {
+        //creates the quadratic bezier curve
+        points = new Vector3[3];
+        points[0] = start;
+        points[1] = bPoint;
+        points[2] = end;
+    }
+
+    public Vector3 getPoint(float t)
+    {
+        return (Bezier.GetPoint(points[0], points[1], points[2], t));
+    }
+
+    public Vector3 getVelocity(float t)
+    {
+        return (getFirstDerivative(points[0], points[1], points[2], t));
+    }
+    
+    //gives a point from the functoin B(t) as defined in the header comment
+    public static Vector3 getPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        t = Mathf.Clamp01(t);// for 0<t<1
+        float oneMinusT = 1f - t;
+        //B(t) =      (1-t)^2        * P0   + 2     (1-t)      t * P1 + t^2   * p2  for 0<t<1 //each point is in a seperate term
+        return oneMinusT * oneMinusT * p0   + 2f * oneMinusT * t * p1 + t * t * p2;
+    }
+
+    //returns the first derivative of the function B(t). B'(t) as definded in the header
+    public static Vector3 getFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        //B'(t) = 2(1-t)(P0-P1) +2t(P2-P1)
+        return 2f * (1f - t) * (p1 - p0) + 2f * t * (p2 - p1);
+    }
+
+}
--- Assets/Trains/Scripts/TrackManager.cs
+++ Assets/Trains/Scripts/TrackManager.cs
@@ -6,7 +6,7 @@ using System;
 public class TrackManager : MonoBehaviour
 {
     [SerializeField]
-    public Track[] tracks;
+    public TrackData[] tracks;
 
     public int trackDensity = 10;
     private int trackCount = 10;
@@ -33,28 +33,28 @@ public class TrackManager : MonoBehaviour
             t = 0;
             trackNum = (trackNum == trackCount-1)?0:trackNum+1;
         }
-        obj.GetComponent<Transform>().position = tracks[trackNum].GetPoint(t) + Vector3.up;
+        obj.GetComponent<Transform>().position = tracks[trackNum].getPoint(t) + Vector3.up;
     }
 
     private void createTracks()
     {
         float start = 0;
-        tracks = new Track[trackCount];
+        tracks = new TrackData[trackCount];
         for(int i = 0; i < trackCount; i++)
         {
-            tracks[i] = new Track(new Vector3(start,0,0),new Vector3(start+trackLength/2,0,3),new Vector3(start+trackLength,0,0));
+            tracks[i] = new TrackData(new Vector3(start,0,0),new Vector3(start+trackLength/2,0,3),new Vector3(start+trackLength,0,0));
             start += trackLength;
         }
     }
 
     private void generateTrack()
     {
-        foreach (Track track in tracks)
+        foreach (TrackData track in tracks)
         {
             for(int i = 0; i < trackDensity; i++)
             {
                 GameObject o = Instantiate(obj);
-                o.GetComponent<Transform>().position = track.GetPoint(i*.1f);
+                o.GetComponent<Transform>().position = track.getPoint(i*.1f);
             }
         }
     }
